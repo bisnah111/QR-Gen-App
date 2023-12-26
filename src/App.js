ecbaa815b382ef906a7c6f9ba2ba237fb05216f4
bisnah111 <nathbishal123@gmail.com>
2023-12-27 00:37:44 +0530

    QR generation app

--- src/App.js
+++ src/App.js
@@ -1,25 +1,50 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState } from 'react'; 
+import './App.css'; 
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+function App() { 
+  
+const [temp, setTemp] = useState(""); 
+const [word, setWord] = useState(""); 
+const [qrCode, setQrCode] = useState(""); 
+
+//QR generate API
+useEffect(() => { 
+	setQrCode 
+(`http://api.qrserver.com/v1/create-qr-code/?data=${word}&size=200x200`); 
+}, [word]); 
+
+//Generate button
+function handleClick() { 
+	setWord(temp); 
+} 
+
+return ( 
+	<div className = "App" > 
+	<h1>Text to QR Convertor</h1> 
+  
+	<div className = "input-box" > 
+		<div className = "gen" > 
+		<input type = "text" onChange={ 
+			(e) => {setTemp(e.target.value)}} 
+			placeholder = "Enter text to convert" /> 
+		</div>
+	</div> 
+
+  <div className = "button" >
+    <button
+			onClick = {handleClick} > 
+			Generate 
+		</button> 
+  </div>
+
+	<div className = "output-box" >  
+    <a href = {qrCode} rel = "noreferrer" >
+        <img src = {qrCode} alt = "" ></img>
+      </a>
+	</div> 
+
+	</div> 
+); 
+} 
 
 export default App;
